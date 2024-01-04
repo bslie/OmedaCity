7b1e8ef4cb061c47544421db7afff57f4346915d
Krovlin Sergey <krovlin97@yandex.ru>
2024-01-04 03:40:48 +0300

    Move Endpoint to a separate Nuget package and remove it from the project

--- OmedaCity/Exceptions/EndpointException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OmedaCity.Exceptions;
-
-public class EndpointException : Exception
-{
-    public EndpointException(string message, Exception innerException)
-        : base(message, innerException)
-    {
-    }
-}
